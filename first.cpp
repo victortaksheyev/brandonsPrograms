3452326adbe1937e7d6fb02b1ece77ac7ecbc4b8
Victor Taksheyev <victortaksheyev@gmail.com>
2019-06-16 22:45:40 -0700

    Add files via upload

--- first.cpp
+++ first.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 
 int main () {
+    string button;
     cout << "this is my first program" << endl;
-
+cin >> button;
+cout << "Hello guys today I will show you how to get minecraft FREE *as Derude Sandstorm plays in the background and i have a cool car as my wallpaper"; 
     return 0;
 }
